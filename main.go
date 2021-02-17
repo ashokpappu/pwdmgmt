5e5b66aadc8a1c09a2027ab6af3b351dec554eca
ashok pappu <ashokpappu@outlook.com>
2021-02-16 23:45:55 -0500

    learnig to add widgets

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
 
 func main() {
 	fmt.Printf("Hello World")
+	a := app.New()
+	w := a.NewWindow("Password Management Tool")
+	hello_w := widget.Label{Text: " Hello Fyne"}
+	widget_button := widget.NewButton("Hi!", func() { hello_w.SetText("Welcome :)") })
+	vboxCntnr := container.NewVBox(widget_button)
+	w.SetContent(vboxCntnr)
+	w.SetCloseIntercept(closeOn)
+	w.ShowAndRun()
+
+}
+func closeOn() {
+	fmt.Printf("close button clicked")
 }
